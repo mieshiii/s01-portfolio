03484aff8a686bd16ea9a8b092567207caaded3b
Mies Eliel Dela Cruz <mieseliel.delacruz@gmail.com>
2021-09-20 21:16:10 +0800

    Add footer component and update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Card from './components/Card'
 import Jumbotron from './components/Jumbo'
 
@@ -18,6 +19,7 @@ export default function App() {
                     <Col><Card/></Col>
                 </Row>
             </Container>
+            <Footer/>
         </>
     )
 }
