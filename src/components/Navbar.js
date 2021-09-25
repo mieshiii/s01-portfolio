0d89e652dfb3d660e77809a27f3e7e4cb6ccfd03
Mies Eliel Dela Cruz <mieseliel.delacruz@gmail.com>
2021-09-25 14:49:34 +0800

    Remove New Nav Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,6 @@ export default function AppNavbar() {
                 <Nav.Link href="#link">Link</Nav.Link>
                 <Nav.Link href="#about">About</Nav.Link>
                 <Nav.Link href="#contactus">Contact Us</Nav.Link>
-                <Nav.Link href="#news">News</Nav.Link>
             </Nav>
         </Navbar>
     )
